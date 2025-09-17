e8befaeaa4fe3d3c0760c1d818202656b91da83c
shubhangi kore <koreshubhangi291@gmail.com>
2025-09-17 14:43:55 +0530

    middlware practices

---
+++ Middleware/app.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const app = express();
+const ExpressError = require("./ExpressError");
+
+const checkToken = (req,res,next)=>{
+    let {token} = req.query;
+    if(token === "giveAccess"){
+       next();
+    }
+   throw new ExpressError(401,"ACCESS DENIED");
+};
+
+app.get("/api",checkToken ,(req,res)=>{
+    res.send("data");
+});
+// app.use((req,res,next)=>{
+//     console.log("Hi, i m 1st middleware");
+//     next();
+// });
+
+// app.use((req,res,next)=>{
+//     console.log("Hi, i m 2nd middleware");
+//     next();
+// });
+
+app.get("/",(req,res)=>{
+    res.send("root page");
+});
+
+app.get("/random",(req,res)=>{
+    res.send("random page");
+});
+
+app.get("/err",(req,res)=>{
+    abcd = abcd;
+});
+
+app.use((err,req,res,next)=>{
+    console.log("ERROR");
+    res.send(err);
+});
+
+//Morgan middleware
+// app.use((req,res,next)=>{
+//     req.time= Date.now();
+//     console.log(req.hostname,req.path,req.method);
+//     next();
+// });
+
+// app.use((req,res)=>{
+//     res.send("page not found");
+// });
+
+app.listen(3000,()=>{
+    console.log("server started at 3000");
+});
